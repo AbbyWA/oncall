4beebf69e1b65450c21c0be5a94424d78245e6d7
footman101 <houyuxin82@gmail.com>
2020-06-23 11:14:49 +0800

    add visitors

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ExposurePlus1Icon from '@material-ui/icons/ExposurePlus1';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import client from '../client';
 import styles from './Home.css';
-import routes from '../constants/routes.json';
+// import routes from '../constants/routes.json';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    // padding: theme.spacing(2),
+    padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
@@ -27,17 +27,24 @@ export default function Home(): JSX.Element {
   const classes = useStyles();
   const [selected, setSelected] = useState();
   const [leaderList, setLeaderList] = useState([]);
-  const [visitorList, setVisitorList] = useState([]);
-
-  const changeSelected = (name) => {
-    setSelected(name);
-  };
+  const [visitors, setVisitors] = useState({});
+  const [newVisitor, setNewVisitor] = useState({
+    name: '',
+    summary: '',
+  });
 
   useEffect(() => {
     client.emit('pull-leader-list');
-    client.on('push-leader-list', (data) => {
-      setLeaderList(data);
-      changeSelected(data[0].name);
+    client.emit('pull-visitor-list');
+
+    client.on('push-leader-list', (payload) => {
+      setLeaderList(payload);
+      setSelected(payload[0].name);
+    });
+    client.on('push-visitor-list', (payload) => {
+      // eslint-disable-next-line no-console
+      console.log(payload);
+      setVisitors(payload);
     });
   }, []);
 
@@ -64,19 +71,71 @@ export default function Home(): JSX.Element {
           </Paper>
         </Grid>
         <Grid item xs={9}>
-          <Paper className={classes.paper}>
+          <Paper
+            className={classes.paper}
+            style={{ paddingTop: '20px', textAlign: 'left' }}
+          >
+            <div>
+              <TextField
+                id="outlined-name"
+                label="来访者姓名"
+                value={newVisitor.name}
+                onChange={(event) => {
+                  setNewVisitor({ ...newVisitor, name: event.target.value });
+                }}
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-summary"
+                label="事由"
+                value={newVisitor.summary}
+                onChange={(event) => {
+                  setNewVisitor({
+                    ...newVisitor,
+                    summary: event.target.value,
+                  });
+                }}
+                variant="outlined"
+                style={{ margin: '0 20px' }}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={!newVisitor.name || !newVisitor.summary}
+                onClick={() => {
+                  client.emit('add-visitor-by-name', {
+                    name: selected,
+                    payload: {
+                      ...newVisitor,
+                      time: Date.now(),
+                    },
+                  });
+                  setNewVisitor({ name: '', summary: '' });
+                }}
+              >
+                添加
+              </Button>
+            </div>
             <List component="nav" aria-label="secondary mailbox folder">
-              <ListItem button onClick={() => {}}>
-                <ListItemIcon>
-                  <ExposurePlus1Icon />
-                </ListItemIcon>
-                <ListItemText primary="添加" />
-              </ListItem>
-              {visitorList.map(({ name }) => (
-                <ListItem key={name}>
-                  <ListItemText primary={name} />
-                </ListItem>
-              ))}
+              {(visitors[selected] || []).map(
+                ({ name, summary, time }, index) => (
+                  <ListItem key={`${name} ${summary} ${time}`}>
+                    <ListItemText primary={`${name} ${summary} ${time}`} />
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => {
+                        client.emit('delete-visitor-by-name', {
+                          name: selected,
+                          index,
+                        });
+                      }}
+                    >
+                      取消
+                    </Button>
+                  </ListItem>
+                )
+              )}
             </List>
           </Paper>
         </Grid>
--- app/server/index.js
+++ app/server/index.js
@@ -2,21 +2,42 @@ import Server from 'socket.io';
 import Store from 'electron-store';
 import data from './data.json';
 
+const store = new Store();
 const io = new Server(8900, { serveClient: false });
 io.origins('*:*');
+let visitors = store.get('visitors');
+
+function visitorsChange() {
+  store.set('visitors', visitors);
+  io.emit('push-visitor-list', visitors);
+}
+if (!visitors) {
+  visitors = data.leaders.reduce((memo, { name }) => {
+    memo[name] = [];
+    return memo;
+  }, {});
+  visitorsChange();
+}
 
 io.on('connect', (socket) => {
-  console.log('connect!!!');
-
   socket.on('pull-leader-list', () => {
-    console.log('pull-leader-list');
     socket.emit('push-leader-list', data.leaders);
-    io.emit('push-leader-list', data.leaders);
+  });
+
+  socket.on('pull-visitor-list', () => {
+    socket.emit('push-visitor-list', visitors);
+  });
+
+  socket.on('add-visitor-by-name', ({ name, payload }) => {
+    visitors[name].unshift(payload);
+    visitorsChange();
+  });
+
+  socket.on('delete-visitor-by-name', ({ name, index }) => {
+    visitors[name] = [
+      ...visitors[name].slice(0, index),
+      ...visitors[name].slice(index + 1),
+    ];
+    visitorsChange();
   });
 });
-
-const store = new Store();
-console.log('unicorn', store.get('unicorn'));
-
-store.set('unicorn', '🦄');
-store.delete('unicorn');
